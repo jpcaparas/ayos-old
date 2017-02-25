f60f73f1f11cd5d86fa6c1acebfe257995da9b08
JP Caparas <jp@jpcaparas.com>
2017-02-25 18:29:59 +1300

    Exposes post model readonly api endpoints

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use TCG\Voyager\Models\Post;
+
+class PostController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->response(Post::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            return $this->response(Post::findOrFail($id));
+        } catch (ModelNotFoundException $e) {
+            $errorMessage = sprintf(
+                'Post with ID: %1$s cannot be found',
+                $id
+            );
+           return $this->errorResponse('not_found', 404, $errorMessage);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+            return $this->errorResponse('bad_request', 400);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group([
+    // Disables auth middleware to expose public, readonly endpoints (e.g. auth)
+    // 'middleware' => 'auth.basic'
+    // todo add oauth2 middleware for POST/PUT/DELETE endpoints
+], function() {
+    Route::resource('/posts', 'Api\PostController', [
+        'except' => [
+            'create', 'store', 'edit', 'update', 'destroy' // Disable POST verbs, READONLY
+        ]
+    ]);
+});
